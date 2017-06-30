9c1efcaa676616462591a05f3fe2ef1d26397bfc
Tao4free <takizawa_tao@live.com>
2017-06-30 13:11:20 +0900

    Add files via upload

---
+++ SuperLabeling/share.py
@@ -0,0 +1,12 @@
+##################################################################################
+#
+#           Module for transfer global variable from map canvas
+#
+##################################################################################
+xPosition = 0.0
+yPosition = 0.0
+found_featureLayername = "Somelayer"
+found_featureID = 0
+field_list = []
+doubleClickCount = 0
+flag_showLb = 1
