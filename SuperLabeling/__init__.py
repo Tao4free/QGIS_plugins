5d216847c8e99c5f506e95c01e7f01b5e24a7668
Tao4free <takizawa_tao@live.com>
2017-07-06 13:03:07 +0900

    Delete __init__.py

--- SuperLabeling/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
- This script initializes the plugin, making it known to QGIS.
-"""
-
-def classFactory(iface):
-    from SuperLabeling import SuperLabeling
-    return SuperLabeling(iface)
